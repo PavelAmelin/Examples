24873637cf2faa2f343837ffe81cbd705bc1c8d8
Pavel <sivec_p@mail.ru>
2023-01-23 01:17:20 +0300

    Add Example012 Sorting

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿void Sort(int[] array)
+{   
+    for (int i = 0; i < array.Length; i++)
+    {
+        int min_pos = i;
+        for (int j = i; j < array.Length; j++)
+        {
+            if (array[j] < array[min_pos])
+            min_pos = j;
+        }
+        int temp = array[i];
+        array[i] = array[min_pos];
+        array[min_pos] = temp;
+        Console.WriteLine($"{array[i]}");
+    }
+}
+
+int[] arr = {5, 3, 4, 53, 2, 2, 3, 4, 42, 1345, 66};
+Sort(arr);
